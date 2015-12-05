bd935aca87a41b82becbfb95fb2a6f37aefb642e
brutalibre <val.isautier@gmail.com>
2015-12-05 13:54:44 +0100

    HanoiTest complete :+1:

--- TP2_Qualite_Logicielle/src/Test/Hanoi_Tests.java
+++ TP2_Qualite_Logicielle/src/Test/Hanoi_Tests.java
@@ -22,4 +22,85 @@ public class Hanoi_Tests {
 		assertArrayEquals(stack.getStackInt(), ht.getTower(0).getStackInt());
 	}
 	
+	@Test
+	public void moveSuccessTest() throws IllegalStateException {
+		int size=3;
+		
+		int[] array1 = {3,2};
+		Stack stack1 = new Stack(size, array1);
+		int[] array2 = {1};
+		Stack stack2 = new Stack(size, array2);
+		
+		HanoiTower ht = new HanoiTower(size);
+		
+		ht.move(0, 1);
+		
+
+		assertArrayEquals(stack1.getStackInt(), ht.getTower(0).getStackInt());
+		assertArrayEquals(stack2.getStackInt(), ht.getTower(1).getStackInt());
+	}
+	
+	@Test
+	public void moveFailureEmptyTowerTest() throws IllegalStateException {
+		int size=0;
+		
+		String message=null;
+		
+		HanoiTower ht = new HanoiTower(size);
+		
+		try {
+			ht.move(0, 1);
+		}
+		catch (IllegalStateException ex){
+			message = ex.getMessage();
+		}
+		
+		assertEquals(message, "Your departure tower is empty, there is no disk to move.");
+	}
+	
+	@Test
+	public void moveFailureSmallerDiskTest() throws IllegalStateException {
+		int size=3;
+		
+		String message=null;
+		
+		HanoiTower ht = new HanoiTower(size);
+		
+		try {
+			ht.move(0, 1);
+			ht.move(0, 1);
+		}
+		catch (IllegalStateException ex){
+			message = ex.getMessage();
+		}
+		
+		assertEquals(message, "You cannot place a disk on a smaller one. Try something else.");
+	}
+	
+	@Test
+	public void victorySuccessTest() throws IllegalStateException {
+		int size = 3;
+		HanoiTower ht = new HanoiTower(size);
+		
+		ht.move(0, 2);
+		ht.move(0, 1);
+		ht.move(2, 1);
+		ht.move(0, 2);
+		ht.move(1, 0);
+		ht.move(1, 2);
+		ht.move(0, 2);
+		
+		assertTrue(ht.checkVictory());
+	}
+	
+	@Test
+	public void victoryFailureTest() throws IllegalStateException {
+		int size = 3;
+		HanoiTower ht = new HanoiTower(size);
+		
+		ht.move(0, 2);
+		
+		assertFalse(ht.checkVictory());
+	}
+	
 }
